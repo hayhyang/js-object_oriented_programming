75d819e6c06e7ba2507b492d123700279856fd3a
hay <hayhyang@gmail.com>
2022-11-23 21:05:54 +0900

    chore: class constructor

--- class.js
+++ class.js
@@ -1,5 +1,12 @@
-class Person {}
+class Person {
+  // 객체가 생성될때 초기상태를 만들어주기 위해 실행되는 메서드
+  constructor(name, first, second) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+  }
+}
 
-const kim = new Person();
+const kim = new Person("kim", 10, 20);
 
 console.log("kim", kim);
