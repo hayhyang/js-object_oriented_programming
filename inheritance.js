1bc8bd610edd7c32073f35daf8f1b8a297445e76
hay <hayhyang@gmail.com>
2022-11-23 21:23:58 +0900

    chore: npm init

--- inheritance.js
+++ inheritance.js
@@ -1,8 +1,7 @@
 // 상속
-// 왜 필요한가?
-// 어떤걸 할 수 있는가?
+// 왜 필요한가? 모든 객체에 사용되지 않는 메서드를 추가할 때 반복되는 코드 없이 추가할 수 있다.
 
-import Person from "./class";
+import Person from "./class.js";
 
 class PersonPlus extends Person {
   avg() {
